f109d0034301e56139b497115382f87ccd2483cc
Nathan Gabriel <nategabrielk@icloud.com>
2024-08-25 21:01:08 -0400

    added some work relating to metadata entity file

--- finance_complaint/entity/metadata_entity.py
+++ finance_complaint/entity/metadata_entity.py
@@ -1,4 +1,11 @@
 import os, sys
 
 from finance_complaint.exception import CustomException
+from finance_complaint.logger import logging
+from collections import namedtuple
+from finance_complaint.utils import read_yaml_file, write_yaml_file
+
+
+DataIngestionMetadataInfo = namedtuple("DataIngestionMetadataInfo", ["from_date", "to_date", "data_file_path"])
+
 
